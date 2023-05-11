7891cb4478e18f7b9cd2d82646810f66b0f93d8a
IT20230692 <IT20230692@my.sliit.lk>
2023-05-11 20:51:29 +0530

    edited

--- src/portfolio.js
+++ src/portfolio.js
@@ -38,7 +38,7 @@ const socialMediaLinks = {
   gmail: "sanjusooriyaarachchi@gmail.com",
   // gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/profile.php?id=100007044557312",
-  // medium: "https://medium.com/@saadpasta",
+
   // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
